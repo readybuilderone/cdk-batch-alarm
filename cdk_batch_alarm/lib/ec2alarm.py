ebb6bf52daa2bbfeaa0ad171fd2a857defcbaf78
readybuilderone <neohan2016@outlook.com>
2022-05-15 18:07:46 +0800

    add status check method

--- cdk_batch_alarm/lib/ec2alarm.py
+++ cdk_batch_alarm/lib/ec2alarm.py
@@ -47,6 +47,81 @@ class EC2StatusCheckFailedAlarm(Construct):
             )
 
 
+class EC2StatusCheckFailedInstanceAlarm(Construct):
+    def __init__(self, scope: Construct, id: str,instanceID: str, instanceName:str, cloudwatchAlarmTopic:sns.ITopic = None, cloudwatchOKAlarmTopic: sns.ITopic = None, cloudwatchInsDataAlarmTopic: sns.ITopic = None, threshold=1, period=60, evaluationPeriods=1, statistic='max', operator = cloudwatch.ComparisonOperator.GREATER_THAN_THRESHOLD, dtm = cloudwatch.TreatMissingData.BREACHING, **kwargs):
+        super().__init__(scope, id, **kwargs)
+
+        metric = cloudwatch.Metric(
+            namespace='AWS/EC2',
+            metric_name='StatusCheckFailed_Instance',
+            dimensions_map={
+                'InstanceId': instanceID
+            }
+        )
+
+        alarm = cloudwatch.Alarm(self, 'scfalarm'+instanceID, 
+            alarm_name= instanceName+'-SCF-ins-'+'Alarm',
+            threshold= threshold,
+            evaluation_periods=evaluationPeriods,
+            metric=metric.with_(
+                period=Duration.seconds(period),
+                statistic=statistic
+            ),
+            comparison_operator=operator,
+            treat_missing_data = dtm,
+        )
+
+        if cloudwatchAlarmTopic:
+            alarm.add_alarm_action(
+                cw_actions.SnsAction(cloudwatchAlarmTopic)
+            )
+        if cloudwatchOKAlarmTopic:
+            alarm.add_ok_action(
+                cw_actions.SnsAction(cloudwatchOKAlarmTopic)
+            )
+        if cloudwatchInsDataAlarmTopic:
+            alarm.add_insufficient_data_action(
+                cw_actions.SnsAction(cloudwatchInsDataAlarmTopic)
+            )
+
+class EC2StatusCheckFailedSystemAlarm(Construct):
+    def __init__(self, scope: Construct, id: str,instanceID: str, instanceName:str, cloudwatchAlarmTopic:sns.ITopic = None, cloudwatchOKAlarmTopic: sns.ITopic = None, cloudwatchInsDataAlarmTopic: sns.ITopic = None, threshold=1, period=60, evaluationPeriods=1, statistic='max', operator = cloudwatch.ComparisonOperator.GREATER_THAN_THRESHOLD, dtm = cloudwatch.TreatMissingData.BREACHING, **kwargs):
+        super().__init__(scope, id, **kwargs)
+
+        metric = cloudwatch.Metric(
+            namespace='AWS/EC2',
+            metric_name='StatusCheckFailed_System',
+            dimensions_map={
+                'InstanceId': instanceID
+            }
+        )
+
+        alarm = cloudwatch.Alarm(self, 'scfalarm'+instanceID, 
+            alarm_name= instanceName+'-SCF-sys-'+'Alarm',
+            threshold= threshold,
+            evaluation_periods=evaluationPeriods,
+            metric=metric.with_(
+                period=Duration.seconds(period),
+                statistic=statistic
+            ),
+            comparison_operator=operator,
+            treat_missing_data = dtm,
+        )
+
+        if cloudwatchAlarmTopic:
+            alarm.add_alarm_action(
+                cw_actions.SnsAction(cloudwatchAlarmTopic)
+            )
+        if cloudwatchOKAlarmTopic:
+            alarm.add_ok_action(
+                cw_actions.SnsAction(cloudwatchOKAlarmTopic)
+            )
+        if cloudwatchInsDataAlarmTopic:
+            alarm.add_insufficient_data_action(
+                cw_actions.SnsAction(cloudwatchInsDataAlarmTopic)
+            )
+
+
 #Ref：https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/viewing_metrics_with_cloudwatch.html
 class EC2CPUUtilizationAlarm(Construct):
 
